de764eac603ff99098512e54109bd4c8705e2428
jiu <jy1001@virnect.com>
2024-09-06 08:57:10 +0900

    test 수정

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 import json
 
+from flask import Flask
+
 from tradingview_screener import Query
 from tradingview_screener.column import Column
+
+app = Flask(__name__)
 
 response = Query().select('close', 'volume', 'EMA5', 'EMA20', 'type').where(Column('close').between(Column('EMA5'), Column('EMA20')),Column('type')
        .isin(['stock'])).order_by('volume', ascending=False).get_scanner_data_raw()
 
+@app.route('/')
+def hello_world():
+    return response['data']
 
+
+@app.route('/array')
+def send_array():
+    return response['data']
 
 data = response['data']
-          
-print(data)           
+
+print(data)
